56c13971715564ec0228a2aeb85c6b2de3e97537
sumitbansode360 <sumitbansode360@gmail.com>
2026-03-19 19:05:18 +0530

    book app setup

--- myproject/book/models.py
+++ myproject/book/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
